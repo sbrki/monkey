4428a3dbf76b35ad7bbf9959cdcc72056ef0712b
Stjepan <13030796+sbrki@users.noreply.github.com>
2022-03-07 19:14:55 +0100

    feat: make parseExpr more readable

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -183,7 +183,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	}
 	leftExpr := prefix()
 
-	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+	for !p.peekTokenIs(token.SEMICOLON) &&  p.peekPrecedence() > precedence{
 		infix := p.infixParseFns[p.peekToken.Type]
 		if infix == nil {
 			return leftExpr
